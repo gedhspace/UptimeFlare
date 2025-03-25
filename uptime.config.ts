807becae1bb0d8cb02e102163e1f2f01220d0250
Gedh-StuffyWalk <103632788+gedhspace@users.noreply.github.com>
2025-03-25 19:12:41 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,6 +1,6 @@
 const pageConfig = {
   // Title for your status page 
-  title: "Gedh's Status Page， 
+  title: "Gedh's Status Page,
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
     { link: 'https://github.com/gedhspace', label: 'GitHub' },
